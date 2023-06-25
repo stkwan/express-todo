c231bf19d5a92e579b18d15086efe418f69f0e05
Steven Kwan <stevent.kwan@gmail.com>
2023-06-25 13:31:59 -0700

    updated cors preflight

--- config/handlePreflight.js
+++ config/handlePreflight.js
@@ -1,7 +1,7 @@
 // CORS preflight OPTIONS request
 const handlePreflight = function (req, res, next) {
   // CORS headers
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Origin", "https://safe-bastion-78041-32acd1b2973d.herokuapp.com"); // restrict it to the required domain
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   // Set custom headers for CORS
   res.header("Access-Control-Allow-Headers", "Content-Type,Accept,X-Custom-Header");
